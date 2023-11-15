59a4ac717cc47c7a148f895ab41179549199dbd1
kbk.local <kbk.local@DESKTOP-INLDRME>
2023-11-15 17:36:08 +0200

    Добавьте файлы проекта.

---
+++ solodkayalab_5.2/Program.cs
@@ -0,0 +1,16 @@
+﻿string inputString = Console.ReadLine();
+string a = "";
+
+for (int i = 0; i < inputString.Length; i++)
+{
+    if (inputString[i] == '!')
+    {
+        a += i.ToString();
+    }
+    else
+    {
+        a += inputString[i];
+    }
+}
+
+Console.WriteLine(a);
